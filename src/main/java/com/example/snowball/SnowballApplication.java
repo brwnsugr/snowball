1b687a79fb8787c86a97d13e3a147ddea9cee539
eunho <eunho@tumblbug.com>
2021-04-08 22:43:00 +0900

    Initial commit

---
+++ src/main/java/com/example/snowball/SnowballApplication.java
@@ -0,0 +1,13 @@
+package com.example.snowball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnowballApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SnowballApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/snowball/SnowballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snowball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnowballApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
