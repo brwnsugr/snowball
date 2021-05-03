443cd00c81dea9519f65e81b7e41b401e4be7916
eunho <eunho@tumblbug.com>
2021-05-04 00:27:29 +0900

    modified deploy script file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -36,5 +36,5 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
   -Dspring.config.location=classpath:/application.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml,classpath:/application-real.yml \
   -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY 2>&1 &
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
 
